310f252ca8227b5ba62ce78d40eb02be73fc961a
sanket <sanketgargate06@gmail.com>
2020-08-24 22:01:40 +0530

    added app styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
+import Sidebar from "./sidebar/Sidebar";
+import Chat from "./chat/Chat";
 
 function App() {
   return (
-    <div className="App">
-     
+    <div className="app">
+
+      <div className="app__body">
+        {/* sidebar */}
+        <Sidebar />
+        {/* chat */}
+        <Chat />
+      </div>
     </div>
   );
 }
