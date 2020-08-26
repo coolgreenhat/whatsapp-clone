ae73e01e44b7fa48551a9aec9032bcf57a594729
sanket <sanketgargate06@gmail.com>
2020-08-26 22:14:47 +0530

    added reducer

---
+++ src/reducer.js
@@ -0,0 +1,24 @@
+const initialState = {
+  user: null,
+};
+ 
+const actionTypes = {
+  SET_USER: "SET_USER",
+};
+
+const reducer = (state, action) => {
+  console.log(action);
+  switch (action.type) {
+    case actionTypes.SET_USER:
+      return {
+        ...state,
+        user: action.user,
+      };
+    
+    default: 
+      return state;
+  }
+};
+
+export { initialState, actionTypes };
+export default reducer;   
