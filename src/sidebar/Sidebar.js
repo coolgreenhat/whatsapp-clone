6f47e84dd74814480c8192cfcfc1d83e37028ea5
sanket <sanketgargate06@gmail.com>
2020-08-26 22:15:57 +0530

    added hooks to sidebar

--- src/sidebar/Sidebar.js
+++ src/sidebar/Sidebar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import "./Sidebar.css";
 import SettingsIcon from '@material-ui/icons/Settings';
 import { Avatar, IconButton } from '@material-ui/core';
@@ -7,41 +7,63 @@ import ChatIcon from "@material-ui/icons/Chat";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import SearchOutlined from "@material-ui/icons/SearchOutlined";
 import SidebarChat from "./sidebarchat/Sidebarchat";
+import { db } from "../firebase";
 
 function Sidebar() {
+    const [rooms, setRooms] = useState([]);
+
+    useEffect(() => {
+      const unsubscribe = db.collection('rooms').onSnapshot((snapshot) => 
+      setRooms(
+        snapshot.docs.map((doc) =>
+          ({
+            id: doc.id,
+            data: doc.data(),
+          })
+        ))
+      );
+
+      return () => {
+        unsubscribe();
+      }
+    }, []);
+
     return (
       <div className="sidebar">
-        {/* <div className="sidebar"> */}
-            <div className="sidebar__header">
-              <Avatar />
-              <div className="sidebar__headerRight">
-                <IconButton>
-                  <DonutLargeIcon />
-                </IconButton>
-                <IconButton>
-                  <ChatIcon />
-                </IconButton>
-                <IconButton>
-                  <MoreVertIcon />
-                </IconButton>
-               </div>
+        <div className="sidebar__header">
+          <Avatar />
+          <div className="sidebar__headerRight">
+            <IconButton>
+              <DonutLargeIcon />
+            </IconButton>
+            <IconButton>
+              <ChatIcon />
+            </IconButton>
+            <IconButton>
+              <MoreVertIcon />
+            </IconButton>
             </div>
+        </div>
 
-            <div className="sidebar__search">
-              <div className="sidebar__searchContainer">
-                <SearchOutlined />
-                <input 
-              placeholder="Search or start new chat"
-              type="text"/> 
-              </div> 
-            </div>
+        <div className="sidebar__search">
+          <div className="sidebar__searchContainer">
+            <SearchOutlined />
+            <input 
+          placeholder="Search or start new chat"
+          type="text"/> 
+          </div> 
+        </div>
             
-            <div className="sidebar__chats">
-              <SidebarChat addNewChat />
-              <SidebarChat />
-              <SidebarChat />
-            </div>
-        {/* </div> */}
+        <div className="sidebar__chats">
+          <SidebarChat addNewChat />
+          {rooms.map(room => (
+            <SidebarChat 
+            key={room.id}
+            id={room.id}
+            name={room.data.name}
+            />
+          ))}
+        </div>
       </div>
     );
 }
