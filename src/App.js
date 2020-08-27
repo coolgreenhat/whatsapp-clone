4b3fb0c2c918cb17661e19f9e35d24018887c0e9
sanket <sanketgargate06@gmail.com>
2020-08-27 21:05:40 +0530

    added react router

--- src/App.js
+++ src/App.js
@@ -2,17 +2,39 @@ import React from 'react';
 import './App.css';
 import Sidebar from "./sidebar/Sidebar";
 import Chat from "./chat/Chat";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Login from './Login';
+import { useStateValue } from './StateProvider';
 
 function App() {
+
+  const [{ user }, dispatch] = useStateValue();
+
   return (
     <div className="app">
 
-      <div className="app__body">
-        {/* sidebar */}
-        <Sidebar />
-        {/* chat */}
-        <Chat />
-      </div>
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+          <Router>
+            <Sidebar />
+
+            <Switch>
+              <Route path="/rooms/:roomId">
+                <Chat />
+              </Route>
+              <Route path="/">
+                <Chat />
+              </Route>
+            </Switch>
+          </Router>
+        </div>
+      )}
     </div>
   );
 }
